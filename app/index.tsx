092dcefaaf3b0a309276bd818b449b34ab01cd6e
coffeecliff <cauaneves976@gmail.com>
2026-04-28 14:28:29 -0300

    commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+export default function Index() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10 }}>
+            <Text>Welcome MeetStranger</Text>
+            <Link href="/home" asChild>
+                <Pressable style={{ padding: 10, backgroundColor: '#000' }}>
+                    <Text style={{ color: '#fff' }}>Ir para Home</Text>
+                </Pressable>
+            </Link>
+            <Link href="/auth/login" asChild>
+                <Pressable style={{ padding: 10, backgroundColor: '#000' }}>
+                    <Text style={{ color: '#fff' }}>Ir para Login</Text>
+                </Pressable>
+            </Link>
+        </View>
+    );
+}
